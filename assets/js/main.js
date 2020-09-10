10d1798feff18bda7ae513c690e8bf84288d5d49
Oscar Giraldo <oscardan@utp.edu.co>
2020-09-10 15:48:11 -0500

    banner principal

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 0) {
+            $('nav').addClass('nav2');
+        }else{
+            $('nav').removeClass('nav2');
+        }
+    });
+});
