e359bdee5354a77f1b087d1bf1b66450f61e9e28
Oscar Giraldo <oscardan@utp.edu.co>
2020-09-12 12:20:57 -0500

    header section-banner menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,14 @@ $(document).ready(function(){
         }
     });
 });
+
+window.onscroll = function(){
+    if (document.documentElement.scrollTop > 100) {
+        document.querySelector('.go-top-container')
+        .classList.add('show');        
+    }else{
+        document.querySelector('.go-top-container')
+        .classList.remove('show');
+    }
+}
+
